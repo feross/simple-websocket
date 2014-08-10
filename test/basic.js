3bd305e2905f2a7d1bb4eb3e4438d1bd8e0b9dae
Feross Aboukhadijeh <feross@feross.org>
2014-08-09 18:21:33 -0700

    use correct protocol over https

--- test/basic.js
+++ test/basic.js
@@ -2,7 +2,8 @@ var Socket = require('../')
 var test = require('tape')
 
 test('basic echo test', function (t) {
-  var socket = new Socket('ws://echo.websocket.org')
+  var proto = window.location.protocol === 'https:' ? 'wss://' : 'ws://'
+  var socket = new Socket(proto + 'echo.websocket.org')
   socket.on('ready', function () {
     t.pass('ready emitted')
     socket.send('sup!')
