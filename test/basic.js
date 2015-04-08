695e85aee0c5888a8c94dd26685f77024cf35cf7
Feross Aboukhadijeh <feross@feross.org>
2015-04-08 23:48:20 +1200

    tests: add Buffer and ArrayBuffer echo tests

--- test/basic.js
+++ test/basic.js
@@ -3,7 +3,7 @@ var test = require('tape')
 
 var SOCKET_SERVER = 'wss://echo.websocket.org'
 
-test('echo text', function (t) {
+test('echo string', function (t) {
   t.plan(3)
 
   var socket = new Socket(SOCKET_SERVER)
@@ -20,13 +20,49 @@ test('echo text', function (t) {
   })
 })
 
-test('echo binary data', function (t) {
+test('echo Uint8Array', function (t) {
   t.plan(4)
 
   var socket = new Socket(SOCKET_SERVER)
   socket.on('ready', function () {
     t.pass('ready emitted')
     socket.send(new Uint8Array([1, 2, 3]))
+    socket.on('message', function (data) {
+      t.ok(Buffer.isBuffer(data), 'data is Buffer')
+      t.deepEqual(data, new Buffer([1, 2, 3]), 'got correct message')
+
+      socket.destroy(function () {
+        t.pass('destroyed socket')
+      })
+    })
+  })
+})
+
+test('echo Buffer', function (t) {
+  t.plan(4)
+
+  var socket = new Socket(SOCKET_SERVER)
+  socket.on('ready', function () {
+    t.pass('ready emitted')
+    socket.send(new Buffer([1, 2, 3]))
+    socket.on('message', function (data) {
+      t.ok(Buffer.isBuffer(data), 'data is Buffer')
+      t.deepEqual(data, new Buffer([1, 2, 3]), 'got correct message')
+
+      socket.destroy(function () {
+        t.pass('destroyed socket')
+      })
+    })
+  })
+})
+
+test('echo ArrayBuffer', function (t) {
+  t.plan(4)
+
+  var socket = new Socket(SOCKET_SERVER)
+  socket.on('ready', function () {
+    t.pass('ready emitted')
+    socket.send(new Uint8Array([1, 2, 3]).buffer)
     socket.on('message', function (data) {
       t.ok(Buffer.isBuffer(data), 'data is Buffer')
       t.deepEqual(data, new Buffer([1, 2, 3]), 'got correct message')
