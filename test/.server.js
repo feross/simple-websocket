f27854fe726b44b92ab3c392a954280bc53c5ad0
Feross Aboukhadijeh <feross@feross.org>
2018-03-03 17:19:12 -0800

    add debug logs

--- test/.server.js
+++ test/.server.js
@@ -1,4 +1,8 @@
 // Start an echo server for airtap browser tests
 
+console.log('airtap port is', process.env.AIRTAP_PORT)
+
 var common = require('./common')
-common.createEchoServer()
+var server = common.createEchoServer(function () {
+  console.log('listening on port', server.address().port)
+})
